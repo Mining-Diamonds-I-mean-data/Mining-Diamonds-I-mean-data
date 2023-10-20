a1dba35c15067b8c3fd87a9cfd09f18cac4272d4
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2023-10-20 11:07:30 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import itertools
 import os
 import random
+from time import sleep
 
 import psycopg2
 from typing import Iterable
@@ -44,6 +45,8 @@ def collect_representative_versions(lib_versions: Iterable[str]):
 # check if package fits within criteria and if it does return the versions to process
 def check_if_we_care(package_name, api_key):
     try:
+        # sleep 1 second just to be safe
+        sleep(1)
         response = requests.get("https://libraries.io/api/Pypi/" + str(
             package_name.split("=")[0]) + "?api_key=" + api_key).json()
         if response["dependents_count"] > 0 and response["dependent_repos_count"] > 0:
